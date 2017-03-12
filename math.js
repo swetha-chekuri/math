47756f1294db33a3eb24c4f5fbe271f0993f8fef
swetha-chekuri <swethasai.chekuri@gmail.com>
2017-03-12 17:36:06 -0500

    Add files via upload

---
+++ math.js
@@ -0,0 +1,19 @@
+function c(val)
+		{
+			document.getElementById("display").value=val;
+		}
+		function math(val)
+		{
+			document.getElementById("display").value+=val;
+		}
+		function e()
+		{
+			try
+				{
+					c(eval(document.getElementById("display").value))
+				}
+			catch(e)
+				{
+					c('Error')
+				}
+		}
